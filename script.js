e8c372ea79981f95cf8a8f7579ca98f6bf00c8fe
Alexandre Canivet <canivet.alexandre@yahoo.fr>
2025-02-10 14:04:56 +0100

    optimisation du code pour le rendre plus facile à lire et modifier

--- script.js
+++ script.js
@@ -1,83 +1,23 @@
-// DECLARATION de la fonction Valider RDV avec cinqs paramètres : heure et minutes du début du RDV ainsi que heure et minutes de fin de journée et la duréee du RDV
+function validateAppointment(startHour, startMinute, endHour, endMinute, duration) {
+    
+    const startTimeInMinutes = convertToMinutes(startHour, startMinute);
+    const endTimeInMinutes = convertToMinutes(endHour, endMinute);
+    const maxPossibleDuration = endTimeInMinutes - startTimeInMinutes;
+    const isAppointmentPossible = duration <= maxPossibleDuration;
+    const appointmentEndTime = convertToTimeFormat(startTimeInMinutes + duration);
 
-function validateAppointment(startAppointmentHour, startAppointmentMinute, endOfDayHour, endOfDayMinute, duration){
-    
+    return isAppointmentPossible 
+        ? `Appointment possible, it will end at ${appointmentEndTime}`
+        : `Appointment impossible, it would end at ${appointmentEndTime}`;
+}
 
-    // DECLARATION des variables de fin du RDV qui serviront à déterminer l'horaire de fin du RDV selon sa durée
-    
-        let endAppointmentHour = startAppointmentHour;
-        let endAppointmentMinute = startAppointmentMinute + duration;
-    
-     // Je cherche à savoir la durée maximale possible pour un RDV, pour cela, je convertis en minutes les horaires de début et de fin de journée puis, je soustrait le nombre de totale de minutes de l'horaire de fin à journée à celui de début de journée
-    
-        let maxDuration = (endOfDayHour * 60 + endOfDayMinute) - (startAppointmentHour * 60 + startAppointmentMinute);
-        
-    // La variable si dessous va venir comparer si la durée choisit et égale ou inférieur à la durée maximale possible, pour savoir si le rendez vous et possible ou non
-        let rdvOk = (duration <= maxDuration);
-    
-    // Maintenant je vais venir convertir la durée du RDV au format 24h00
-        let correctFormatDuration = duration;
-        if (correctFormatDuration >= 60){
-            let durationHours = 0;  
-            let durationMinute = correctFormatDuration;
-            while (durationMinute >= 60){
-                durationMinute = durationMinute - 60;
-                durationHours = durationHours + 1;
-                if (durationHours < 10 && durationMinute < 10){
-                    correctFormatDuration = "0" + durationHours + "h0" + durationMinute + "min";
-                } else if (durationHours < 10){
-                    correctFormatDuration = "0" + durationHours + "h" + durationMinute + "min";
-                } else if (durationMinute < 10){
-                    correctFormatDuration = durationHours + "h0" + durationMinute + "min";
-                } else {
-                    correctFormatDuration = durationHours + "h" + durationMinute + "min";
-                }
-            } 
-            
-        } else {
-            correctFormatDuration = duration + "min";
-        }
-    
-    // Pour gérer le cas ou le RDV se termine après 24h
-        while( endAppointmentMinute >= 60){
-            endAppointmentMinute = endAppointmentMinute - 60;
-            endAppointmentHour = endAppointmentHour + 1;
-            while(endAppointmentHour >= 24){
-                endAppointmentHour = endAppointmentHour - 24;
-            }
-        };
-        
-        // Affichage de l'heure de fin du RDV'
-        if (endAppointmentHour < 10 && endAppointmentMinute < 10){
-            if(rdvOk){
-                console.log(correctFormatDuration + ' , RDV Ok, le RDV se terminera à : 0' + endAppointmentHour + "h0" + endAppointmentMinute);
-            } else{
-                console.log(correctFormatDuration + ' , RDV Impossible, le RDV se terminera à : 0' + endAppointmentHour + "h0" + endAppointmentMinute);
-            }    
-        } else if (endAppointmentHour < 10){
-            if(rdvOk){
-                console.log(correctFormatDuration + ' , RDV Ok, le RDV se terminera à : 0' + endAppointmentHour + "h" + endAppointmentMinute);
-            } else {
-                console.log(correctFormatDuration + ' , RDV Impossible, le RDV se terminera à : 0' + endAppointmentHour + "h" + endAppointmentMinute);
-            }
-        } else if (endAppointmentMinute < 10){
-            if (rdvOk){
-                console.log(correctFormatDuration + ' , RDV Ok, le RDV se terminera à : ' + endAppointmentHour + "h0" + endAppointmentMinute);
-            } else {
-                console.log(correctFormatDuration + ' , RDV Impossible, le RDV se terminera à : ' + endAppointmentHour + "h0" + endAppointmentMinute);
-            }
-        } else {
-            if (rdvOk){
-                console.log(correctFormatDuration + ' , RDV Ok, le RDV se terminera à : ' + endAppointmentHour + "h" + endAppointmentMinute);
-            } else{
-                console.log(correctFormatDuration + ' , RDV Impossible, le RDV se terminera à : ' + endAppointmentHour + "h" + endAppointmentMinute);
-            }
-        }
-        
-    }
+function convertToMinutes(hours, minutes) {
+    return hours * 60 + minutes;
+}
 
+function convertToTimeFormat(totalMinutes) {
+    const hours = Math.floor(totalMinutes / 60) % 24;
+    const minutes = totalMinutes % 60;
 
-    // for(let duree = 0 ; duree < 800 ; duree++){
-    //     validateAppointment(16, 30, 17, 0, duree);
-    
-    // }
+    return `${String(hours).padStart(2, "0")}h${String(minutes).padStart(2, "0")}`;
+}
